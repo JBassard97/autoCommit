ebe43c22ad6e264f0bc1e89f5a3796cbd754c96d
Jbassard <jonathanacciarito@gmail.com>
2024-11-08 20:37:34 -0500

    changed a little nit in the textChange.js that made the text file a lil ugly. Very minor, didn't affect performance, but hurt my soul. Also scaled back how often the auto commit action works: from every 30 minutes to 2 hours

--- textChange.js
+++ textChange.js
@@ -10,17 +10,21 @@ function getRandomLetter() {
 // Path to the text.js file
 const textFilePath = path.join(__dirname, "text.js");
 
-// Read the current content of text.js
-let currentText = fs
-  .readFileSync(textFilePath, "utf8")
-  .replace('const text = "', "")
-  .replace('";\n\nmodule.exports = text;\n', "");
+// Read only the text value in text.js
+let currentText = "";
+try {
+  currentText = fs
+    .readFileSync(textFilePath, "utf8")
+    .match(/const text = "(.*)";/)[1]; // Extract only the content between the quotes
+} catch (error) {
+  console.error("Error reading text.js:", error.message);
+}
 
 // Add a random letter to the text
 const randomLetter = getRandomLetter();
 const newText = currentText + randomLetter;
 
-// Write the new content back to text.js
+// Write the clean new content back to text.js
 const newContent = `const text = "${newText}";\n\nmodule.exports = text;\n`;
 fs.writeFileSync(textFilePath, newContent, "utf8");
 
